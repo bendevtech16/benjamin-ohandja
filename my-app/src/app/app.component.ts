ab77e01587debc68b6b75854452ab49afe7c96e9
benjamin OHANDJA <bendevtech16@gmail.com>
2023-09-07 01:07:09 +0100

    suites de mes outils et footer
    jai ajouter mes projets et le footer sansoublier les outils

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Carousel, Dropdown, initTE, Tab, Chip} from 'tw-elements';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  ngOnInit() {
+    initTE({ Carousel, Dropdown, Tab,Chip});
+  }
   title = 'my-app';
 }
